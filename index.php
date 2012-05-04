361bf3bfb4e1eee0f2b9d68c0f9ee78b03bc412e
Stefano Sabatini <stefasab@gmail.com>
2012-05-04 15:29:59 +0200

    iching: apply style fix

--- index.php
+++ index.php
@@ -32,19 +32,21 @@
     </form>
 
     <form name="questionForm" id="questionForm" action="getAnswer.php" method="post">
-      <p class="style2">
+      <div class="style2">
         Type your question here<br>
         <textarea cols="40" rows="5" id="questionTextArea">
         </textarea>
         <br>
-      
+
         <input type="submit" value="Get answer">
         <input type="button" value="Clear question"
                onclick="$('#questionTextArea').val('');">
 
         <!-- see which IChing systems are available -->
+        <div>
         <?php include "get_iching_radio.php" ?>
-      </p>
+        </div>
+      </div>
     </form>
   </div>
 
